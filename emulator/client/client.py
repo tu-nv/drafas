92538e417a6e5beed1526f22f39c5188d73be92b
tu-nv <vantu.bkhn@gmail.com>
2024-10-27 13:08:54 +0000

    add sla violation and avg processing time computation to client

--- emulator/client/client.py
+++ emulator/client/client.py
@@ -14,10 +14,94 @@ from fastapi import FastAPI
 app = FastAPI()
 
 results = []
-results_lock = multiprocessing.Lock()
+results_lock = asyncio.Lock()
 failed_requests = []
+failed_requests_lock = asyncio.Lock()
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+parser = argparse.ArgumentParser(description='Asyncio HTTP Requests Script')
+# parser.add_argument('--url', type=str, default="http://141.223.124.62:8080", help='The URL to send requests to')
+parser.add_argument('--plan_file', type=str, default="plan.txt", help='relative path to the plan file')
+parser.add_argument('--test_case', type=str, default="whisper",
+                    help='test case, choose between ollama, triton, whisper, emulator-ollama, emulator-triton, emulator-whisper')
+parser.add_argument('--speedup', type=int, default=1, help='speed up factor')
+args = parser.parse_args()
+
+request_data = {
+        "url": None,
+        "file_content": None,
+        "json": None,
+        "headers": None
+    }
+
+if args.test_case == 'triton':
+    delay_sla = 1.0
+    request_data['url'] = "http://141.223.124.62:30800/v2/models/resnet50/infer"
+    img = Image.open(f"{BASE_DIR}/data/cup.jpg")
+    # preprocessed img, FP32 formated numpy array
+    data = preprocess_img(img, "float32", 224, 224)
+    # data = data.tobytes()
+    request_data["json"] = {
+            "inputs": [
+                {
+                "name": "data",
+                "shape": [1, 3, 224, 224],
+                "datatype": "FP32",
+                "data": data.tolist()
+                }
+            ],
+            "outputs": [
+                {
+                "name": "resnetv24_dense0_fwd",
+                "parameters": { "classification" : 3 }
+                }
+            ]
+        }
+    # test inference
+    # response = requests.post(request_data['url'], json=request_data["json"])
+    # print(response.text)
+    # exit(1)
+elif args.test_case == 'ollama':
+    delay_sla = 2.0
+    request_data['url'] = "http://141.223.124.62:30434/api/generate"
+    request_data['json'] = {
+                "model": "llama3.2:1b-instruct-q4_K_M",
+                "prompt": "How many r in strawberry?",
+                "options": {
+                    "num_gpu": 99
+                },
+                    "stream": False
+            }
+elif args.test_case == 'whisper':
+    delay_sla = 2.0
+    request_data['url'] = "http://141.223.124.62:30900/asr?output=json"
+    request_data['headers'] = {'content-type': 'multipart/form-data'}
+    with open(f"{BASE_DIR}/data/eng_male.wav", "rb") as audio_file:
+        request_data['file_content'] = audio_file.read()
+    # exit(0)
+elif args.test_case == 'emulator-triton':
+    delay_sla = 1.0
+    request_data['url'] = "http://localhost:8102/process_request"
+    request_data['json'] = {
+                "message": "hello emulator",
+            }
+elif args.test_case == 'emulator-ollama':
+    delay_sla = 2.0
+    request_data['url'] = "http://localhost:8101/process_request"
+    request_data['json'] = {
+                "message": "hello emulator",
+            }
+elif args.test_case == 'emulator-whisper':
+    delay_sla = 2.0
+    request_data['url'] = "http://localhost:8103/process_request"
+    request_data['json'] = {
+                "message": "hello emulator",
+            }
+else:
+    print(f"Wrong test case: {args.test_case}")
+    exit(1)
+
 
 async def schedule_requests(request_data, num_requests, delay_bw_request, session: aiohttp.ClientSession):
     tasks = []
@@ -44,14 +128,17 @@ async def post_request(delay_before_request, request_data, session: aiohttp.Clie
             # print(response_dat)
     except asyncio.TimeoutError:
         print(f"Request timed out after {timeout} seconds")
-        failed_requests.append('Timeout')
+        async with failed_requests_lock:
+            failed_requests.append('Timeout')
     except Exception as e:
         print(f"Request failed: {e}")
-        failed_requests.append(e)
+        async with failed_requests_lock:
+            failed_requests.append(e)
     else:
         end = time.time()
-        elapsed = end - start
-        results.append(elapsed)
+        processing_time = end - start
+        async with results_lock:
+            results.append([time.time(), processing_time])
 
 async def load_plan(file_path):
     plan = []
@@ -66,27 +153,42 @@ async def load_plan(file_path):
     return plan
 
 async def cal_results():
-    if results:
-        average_time = sum(results) / len(results)
-        min_time = min(results)
-        max_time = max(results)
-        print(f"Number of success requests: {len(results)}")
-        print(f"Average time: {average_time:.4f} seconds")
-        print(f"Min time: {min_time:.4f} seconds")
-        print(f"Max time: {max_time:.4f} seconds")
-    else:
-        print("No successful requests to report.")
-
-    if failed_requests:
-        print(f"{len(failed_requests)} requests failed.")
-    else:
-        print("No requests failed.")
+    async with results_lock:
+        if len(results) > 0:
+            processing_times = [x[1] for x in results]
+            average_time = sum(processing_times) / len(processing_times)
+            min_time = min(processing_times)
+            max_time = max(processing_times)
+            print(f"Number of success requests: {len(processing_times)}")
+            print(f"Average time: {average_time:.4f} seconds")
+            print(f"Min time: {min_time:.4f} seconds")
+            print(f"Max time: {max_time:.4f} seconds")
+        else:
+            print("No successful requests to report.")
+    async with failed_requests_lock:
+        if failed_requests:
+            print(f"{len(failed_requests)} requests failed.")
+        else:
+            print("No requests failed.")
 
 @app.get("/stats")
 async def get_stats():
+    now = time.time()
+    sla_violation_rate = 0.0
+    avg_processing_time_s = 0.0
+    async with results_lock:
+        if len(results) > 0:
+            # only take results of last 15 secs
+            recent_proc_times = [x[1] for x in results if (now - x[0]) * args.speedup <= 15]
+            if len(recent_proc_times) > 0:
+                sla_violations = [x for x in recent_proc_times if x > delay_sla]
+                sla_violation_rate = len(sla_violations) / len(recent_proc_times)
+                avg_processing_time_s = sum(recent_proc_times) / len(recent_proc_times)
     return {
-        "sla_violation_rate": 0
+        "sla_violation_rate": sla_violation_rate,
+        "avg_processing_time_s": avg_processing_time_s
     }
+
 async def run_app():
     config = uvicorn.Config(app=app, host="0.0.0.0", port=8200, loop="asyncio", lifespan="off")
     server = uvicorn.Server(config)
@@ -94,84 +196,8 @@ async def run_app():
     await server.serve()
 
 async def main():
-    parser = argparse.ArgumentParser(description='Asyncio HTTP Requests Script')
-    # parser.add_argument('--url', type=str, default="http://141.223.124.62:8080", help='The URL to send requests to')
-    parser.add_argument('--plan_file', type=str, default="plan.txt", help='relative path to the plan file')
-    parser.add_argument('--test_case', type=str, default="whisper",
-                        help='test case, choose between ollama, triton, whisper, emulator-ollama, emulator-triton, emulator-whisper')
-    parser.add_argument('--speed_up', type=int, default=1, help='speed up factor')
-    args = parser.parse_args()
-
     server_task = asyncio.create_task(run_app())
     await asyncio.sleep(1)
-
-    request_data = {
-        "url": None,
-        "file_content": None,
-        "json": None,
-        "headers": None
-    }
-
-    if args.test_case == 'triton':
-        request_data['url'] = "http://141.223.124.62:30800/v2/models/resnet50/infer"
-        img = Image.open(f"{BASE_DIR}/data/cup.jpg")
-        # preprocessed img, FP32 formated numpy array
-        data = preprocess_img(img, "float32", 224, 224)
-        # data = data.tobytes()
-        request_data["json"] = {
-                "inputs": [
-                    {
-                    "name": "data",
-                    "shape": [1, 3, 224, 224],
-                    "datatype": "FP32",
-                    "data": data.tolist()
-                    }
-                ],
-                "outputs": [
-                    {
-                    "name": "resnetv24_dense0_fwd",
-                    "parameters": { "classification" : 3 }
-                    }
-                ]
-            }
-        # test inference
-        # response = requests.post(request_data['url'], json=request_data["json"])
-        # print(response.text)
-        # exit(1)
-    elif args.test_case == 'ollama':
-        request_data['url'] = "http://141.223.124.62:30434/api/generate"
-        request_data['json'] = {
-                    "model": "llama3.2:1b-instruct-q4_K_M",
-                    "prompt": "How many r in strawberry?",
-                    "options": {
-                        "num_gpu": 99
-                    },
-                        "stream": False
-                }
-    elif args.test_case == 'whisper':
-        request_data['url'] = "http://141.223.124.62:30900/asr?output=json"
-        request_data['headers'] = {'content-type': 'multipart/form-data'}
-        with open(f"{BASE_DIR}/data/eng_male.wav", "rb") as audio_file:
-            request_data['file_content'] = audio_file.read()
-        # exit(0)
-    elif args.test_case == 'emulator-ollama':
-        request_data['url'] = "http://localhost:8101/process_request"
-        request_data['json'] = {
-                    "message": "hello emulator",
-                }
-    elif args.test_case == 'emulator-triton':
-        request_data['url'] = "http://localhost:8102/process_request"
-        request_data['json'] = {
-                    "message": "hello emulator",
-                }
-    elif args.test_case == 'emulator-whisper':
-        request_data['url'] = "http://localhost:8103/process_request"
-        request_data['json'] = {
-                    "message": "hello emulator",
-                }
-    else:
-        print(f"Wrong test case: {args.test_case}")
-        exit(1)
 
     plan = await load_plan(f'{BASE_DIR}/{args.plan_file}')
 
